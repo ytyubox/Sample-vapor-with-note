9eb890c90f56ebeb3cf493d6df89d70f7697adfc
Tsungyu Yu <ytyubox@gmail.com>
2020-03-28 21:16:05 +0800

    code implementation done

--- HelloVapor/Sources/App/routes.swift
+++ HelloVapor/Sources/App/routes.swift
@@ -5,7 +5,43 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+	router.get("hello",use: hardCode)
+	router.get("hello","vapor", use: queryed)
+	router.get("hello",String.parameter, use: dynamicPath)
+	router.post(InfoData.self,at:"info", use: handleJSON)
+}
+
+// MARK: - Route handler
+
+public typealias ToResponse<T> = (Request) throws -> T
+public typealias ContentHandler<T:Content,U:Content> = (Request,T) -> U
+
+fileprivate let hardCode: ToResponse =  { req in
+	return "Hello, world!"
+}
+
+fileprivate let queryed:ToResponse = { req in
+	"hello, " +
+		(req.query[String.self,at: "name"] ?? "Vapor")
+}
+
+
+fileprivate let dynamicPath: ToResponse = {
+	req in
+	"hello, " +
+		((try? req.parameters.next(String.self)) ?? "P a t h")
+}
+fileprivate struct InfoData: Content {
+ let name: String
+}
+
+fileprivate struct HelloResponse:Content {
+	let code:Int
+	let message:String
+}
+
+fileprivate let handleJSON:ContentHandler<InfoData,HelloResponse> = {
+	(req, body) -> HelloResponse in
+	let name = body.name
+	return HelloResponse(code: 0, message: "hello, \(name)")
 }
